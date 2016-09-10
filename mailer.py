a3efd8d1915be0d4abc11d68f1cddde9d530c1b0
RobWei <robwei280101@gmail.com>
2016-09-10 19:44:58 +0200

    Create mailer.py

---
+++ mailer.py
@@ -0,0 +1,17 @@
+import smtplib
+ 
+smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
+smtpObj.starttls()
+smtpObj.login('smallfootprojekt@gmail.com','q1w2e3r4t5z6u7i8o9p0')
+ 
+def sendVerify(mail, code):
+    str = str(code)
+    msg = """Subject: SmallFoot-Verifikation \n\n
+   Hello environmentalist!
+ 
+   Your verification code is: """+ str + """
+ 
+   Your SmallFoot-Team
+   """
+    smtpObj.sendmail("smallfootprojekt@gmail.com", mail, msg)
+    print ("Mail Sent at " + mail + ", with code " + code)
